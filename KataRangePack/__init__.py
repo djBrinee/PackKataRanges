1ac2cedcbd6e6ea51b9b939b30bd7a1e7e3f226d
Deivy Jr <deivyjunior12@gmail.com>
2022-06-26 17:11:47 -0400

    Old code to check

---
+++ KataRangePack/__init__.py
@@ -0,0 +1,47 @@
+class RangeClass:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def Contains(self, value):
+        t1 = False
+        var = range(self.first, self.last)
+        for i in var:
+            if(i == value):
+                t1 = True
+        return t1
+
+    def GetAllPoints(self):
+        result = ""
+        var = range(self.first, self.last)
+        for i in var:
+            if i < self.last - 1 :
+                result += str(i) + ","
+            else:
+                result += str(i)
+        return result
+    
+    def EndPoints(self):
+        return str(self.first) + "," + str(self.last-1)
+
+
+    def Equals(self, other: 'RangeClass'):
+        var1 = range(self.first, self.last)
+        var2 = range(other.first, other.last)
+        if(var1 == var2):
+            return True
+        else:
+            return False
+
+    def overlapsRange(self, other: 'RangeClass'):
+        choice = False
+
+        for i in range(self.first, self.last):
+            for j in range(other.first, other.last):
+                if(i == j):
+                    choice = True
+                else:
+                    choide = False
+        return choice
+                
+
---
+++ test_KataRangePack.py
@@ -0,0 +1,38 @@
+import pytest
+from KataRange import RangeClass
+
+c1 = RangeClass(2, 6)
+c2 = RangeClass(2, 6)
+c3 = RangeClass(3, 7)
+c4 = RangeClass(4, 5)
+c5 = RangeClass(7, 10)
+c6 = RangeClass(2, 5)
+c7 = RangeClass(2, 10)
+c8 = RangeClass(3, 5)
+
+def test_RangeContains():
+    assert c1.Contains(2) == True
+
+def test_GetAllPoints():
+    assert c1.GetAllPoints() == "2,3,4,5"
+
+def test_NoContain():
+    assert c2.Contains(100) == False
+    
+def test_EndPoints():
+    assert c1.EndPoints() == "2,5"
+
+def test_Equals():
+    assert c1.Equals(c2) == True
+
+def test_notEquals():
+    assert c3.Equals(c4) == False
+
+def test_NotoverlapsRange():
+    assert c5.overlapsRange(c6) == False
+
+def test_overlapsRange():
+    assert c7.overlapsRange(c8) == True
+
+def test_overlapsRange1():
+    assert c7.overlapsRange(c7) == True

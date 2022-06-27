26e38ed50342b61c924605fc551bdb2a4561d2d8
Deivy Jr <deivyjunior12@gmail.com>
2022-06-26 21:44:52 -0400

    Creating pack

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='KataRangesPack',
+ version='0.1',
+ author='Deivy & Jordi',
+ author_email="1099429@est.intec.edu.do",
+ description="<Basic desc>",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
